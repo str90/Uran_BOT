05f36722588a2f9ac1e3a52b9e168495c5c9bac9
str90 <a.babitckii@gmail.com>
2017-04-13 16:22:18 +0200

    Implement nice command interpeter for HandTrader class

--- src/com/amberrosin/btcbot/HandTrader.java
+++ src/com/amberrosin/btcbot/HandTrader.java
@@ -35,50 +35,52 @@ public class HandTrader {
         forSimpleTrade.getInfo.runMethod();
 
         while (!exit) {
-            System.out.printf("What do you want to do ?\n\n1 - Currency full sell for current market price\n2 - Currency full buy for current market price\n" +
-                    "3 - Get first currency balance\n4 - Get second currency balance\n5 - Get current pair market price\n6 - Get current pair market sell price\n" +
-                    "7 - Get current pair market buy price\n8 - Back to main menu\n$>");
+            System.out.printf("What do you want to do ?\n\n\"fullsell\" - Currency full sell for current market price\n\"fullbuy\" - Currency full buy for current market price\n" +
+                    "\"firstbalance\" - Get first currency balance\n\"secondbalance\" - Get second currency balance\n\"price\" - Get current pair market price\n" +
+                    "\"sellprice\" - Get current pair market sell price\n" +
+                    "\"buyprice\" - Get current pair market buy price\n\"back\" - Back to main menu\n$>");
             Scanner getCurrentAction = new Scanner(System.in);
             while(true) {
                 currentAction = getCurrentAction.nextLine();
-                if(currentAction.equals("1") || currentAction.equals("2") || currentAction.equals("3") || currentAction.equals("4") || currentAction.equals("5") || currentAction.equals("6")
-                        || currentAction.equals("7")) break;
-                else if(currentAction.equals("8")) {
+                if(currentAction.equals("fullsell") || currentAction.equals("fullbuy") || currentAction.equals("firstbalance") || currentAction.equals("secondbalance")
+                        || currentAction.equals("price") || currentAction.equals("sellprice")
+                        || currentAction.equals("buyprice")) break;
+                else if(currentAction.equals("back")) {
                     exit = true;
                     break;
                 }
                 else System.out.printf("Incorrect input, please try again\n$>");
             }
-            if(currentAction.equals("1")) {
+            if(currentAction.equals("fullsell")) {
                 System.out.printf("You`re going to sell all your second currency, proceed ? Y/N\n$>");
                 if(getDecision.equals("Y")) {
                     forSimpleTrade.tryMaximumSell(currentPair);
                 }
                 else System.out.printf("Sell cancelled\n");
             }
-            if(currentAction.equals("2")) {
+            if(currentAction.equals("fullbuy")) {
                 System.out.printf("You`re going to full buy currency, proceed ? Y/N\n$>");
                 if(getDecision.equals("Y")) {
                     forSimpleTrade.tryMaximumBuy(currentPair, reuseMillis);
                 }
             }
-            if(currentAction.equals("3")) {
+            if(currentAction.equals("firstbalance")) {
                 System.out.printf("Your first currency active balance is %s %S\n\n", forSimpleTrade.getInfo.getBalance(currentPairDivided[0]), currentPairDivided[0]);
             }
-            if(currentAction.equals("4")) {
+            if(currentAction.equals("secondbalance")) {
                 System.out.printf("Your second currency active balance is %s %S\n\n", forSimpleTrade.getInfo.getBalance(currentPairDivided[1]), currentPairDivided[1]);
             }
-            if(currentAction.equals("5")) {
+            if(currentAction.equals("price")) {
                 forSimpleTrade.ticker.switchNextPair();
                 System.out.printf("Current pair market price is %s %S\n\n", forSimpleTrade.ticker.getCurrentLast(), currentPairDivided[1]);
                 forSimpleTrade.ticker.switchResetPair();
             }
-            if(currentAction.equals("6")) {
+            if(currentAction.equals("sellprice")) {
                 forSimpleTrade.ticker.switchNextPair();
                 System.out.printf("Current pair market sell price is %s %S\n\n", forSimpleTrade.ticker.getCurrentSell(), currentPairDivided[1]);
                 forSimpleTrade.ticker.switchResetPair();
             }
-            if(currentAction.equals("7")) {
+            if(currentAction.equals("buyprice")) {
                 forSimpleTrade.ticker.switchNextPair();
                 System.out.printf("Current pair market buy price is %s %S\n\n", forSimpleTrade.ticker.getCurrentBuy(), currentPairDivided[1]);
                 forSimpleTrade.ticker.switchResetPair();
